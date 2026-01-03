5000bc1d74f890ffaae942fe8f389a74d3dc3027
Gorkem Korkmaz <gorkeminanckorkmaz@gmail.com>
2026-01-03 17:07:31 +0200

    feat: add GitHub Actions workflows and terraform-docs config

---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.85.1"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
+    }
+  }
+}
